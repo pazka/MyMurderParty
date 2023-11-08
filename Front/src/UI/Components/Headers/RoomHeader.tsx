e7dd3a95ab43647e0c561526b64ceee467ea559f
Alexandre WEISSER <alexandre.weisser@gmail.com>
2023-11-09 00:06:12 +0100

    feat(style) room header

--- Front/src/UI/Components/Headers/RoomHeader.tsx
+++ Front/src/UI/Components/Headers/RoomHeader.tsx
@@ -1,10 +1,12 @@
+import { getCurrentGameConfig } from '../../../services/gameService';
 import { useGlobalStorage } from '../../../services/storageService'
 import './Headers.scss'
 
 export default () => {
     const [storage] = useGlobalStorage();
-    
+    const gameName = getCurrentGameConfig()?.GAME_NAME ?? "Unknown Game"
+
     return <div className="header panel">
-        <h2>{storage.currentRoom?.name}{'<'}{storage.currentRoom?.id}{'>'}</h2>
+        <span className='small-text'><span className='spacer'/> {storage.currentRoom?.name}</span><b>{'['}{storage.currentRoom?.id}{']'} </b><span className='small-text'>{gameName}</span>
     </div>
 }
